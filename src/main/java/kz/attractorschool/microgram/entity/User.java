018acc92b0b5cb8e663fdcf575800a690c5ca868
UlugbekBegaliev <uluga.begaliev@gmail.com>
2021-11-29 18:01:38 +0600

    added class User

--- src/main/java/kz/attractorschool/microgram/entity/User.java
+++ src/main/java/kz/attractorschool/microgram/entity/User.java
@@ -1,4 +1,26 @@
 package kz.attractorschool.microgram.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Document(collection = "users")
 public class User {
+
+    @Id
+    private String id;
+    private String email;
+    private String username;
+    private String password;
+    private Integer countOfPosts = 0;
+    private Integer countOfFollowers = 0;
+    private Integer countOfSubscribe = 0;
+
 }
