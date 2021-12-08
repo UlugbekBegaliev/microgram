1bc618679e9eb900603386401cf98e17eaa36fc8
UlugbekBegaliev <uluga.begaliev@gmail.com>
2021-12-09 04:19:51 +0600

    added first method in controllers

--- src/main/java/kz/attractorschool/microgram/controller/CommentController.java
+++ src/main/java/kz/attractorschool/microgram/controller/CommentController.java
@@ -1,5 +1,23 @@
 package kz.attractorschool.microgram.controller;
 
+import kz.attractorschool.microgram.dto.CommentDTO;
+import kz.attractorschool.microgram.service.CommentService;
+import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/comments")
 public class CommentController {
 
+    private final CommentService commentService;
+
+    @GetMapping
+    public Slice<CommentDTO> findComments(Pageable pageable){
+        return commentService.findAll(pageable);
+    }
 }
