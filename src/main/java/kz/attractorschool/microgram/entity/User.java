b9710f81173a3d45560fd33fbd4dfcd0fb4b0f12
UlugbekBegaliev <uluga.begaliev@gmail.com>
2021-12-16 01:55:30 +0600

    class User implements UserDetails

--- src/main/java/kz/attractorschool/microgram/entity/User.java
+++ src/main/java/kz/attractorschool/microgram/entity/User.java
@@ -4,13 +4,17 @@ import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
 
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PRIVATE, force = true)
 @Document(collection = "users")
 @Data
-public class User {
+public class User implements UserDetails {
 
     @Indexed
     private String username;
@@ -27,5 +31,30 @@ public class User {
         this.fullName = fullName;
         this.email = email;
         this.password = password;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
     }
 }
