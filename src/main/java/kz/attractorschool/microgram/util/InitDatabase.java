7eb229941dd5d8d9029ebd79ee3aa8bbed960f32
UlugbekBegaliev <uluga.begaliev@gmail.com>
2021-12-15 23:50:51 +0600

    added new class InitDatabase and Model Lists

--- src/main/java/kz/attractorschool/microgram/util/InitDatabase.java
+++ src/main/java/kz/attractorschool/microgram/util/InitDatabase.java
@@ -1,54 +1,63 @@
 package kz.attractorschool.microgram.util;
 
+import kz.attractorschool.microgram.entity.*;
+import kz.attractorschool.microgram.repository.*;
 import lombok.AllArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.time.LocalDateTime;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import java.util.ArrayList;
+import java.util.List;
 
 @AllArgsConstructor
 @Configuration
 public class InitDatabase {
 
-    private final EventRepository eventRepository;
-    private final SubscriptionRepository subscriptionRepository;
+    private final PasswordEncoder encoder;
 
-    // раскомменитруйте следующие строки
-    // когда создадите репозиторий пользователя
-    // и сервис авторизации
-//    private final UserRepository userRepository;
-//    private final PasswordEncoder encoder;
+    private final CommentRepository commentRepository;
+    private final LikeRepository likeRepository;
+    private final PublicationRepository publicationRepository;
+    private final SubscriptionRepository subscriptionRepository;
+    private final UserRepository userRepository;
 
     @Bean
-    public CommandLineRunner fillData() {
+    public CommandLineRunner fillData(CommentRepository commentRepository,
+                                      LikeRepository likeRepository,
+                                      PublicationRepository publicationRepository,
+                                      SubscriptionRepository subscriptionRepository,
+                                      UserRepository userRepository) {
         return (args) -> {
+            commentRepository.deleteAll();
+            likeRepository.deleteAll();
+            publicationRepository.deleteAll();;
             subscriptionRepository.deleteAll();
-            eventRepository.deleteAll();
-            eventRepository.save(Event.builder().id("e001").name("Make the shelter").description("Make it sturdy").eventDate(LocalDateTime.now().minusDays(1)).build());
-            eventRepository.save(Event.builder().id("e011").name("Repair the shelter").description("Make it safe and cozy.").eventDate(LocalDateTime.now().plusDays(1)).build());
-            eventRepository.save(Event.builder().id("e012").name("Gather some firewood").description("To keep the fireplace running").eventDate(LocalDateTime.now().plusDays(1)).build());
-            eventRepository.save(Event.builder().id("e013").name("Winter is coming!").description("Survive the winter!").eventDate(LocalDateTime.now().plusDays(1)).build());
+            userRepository.deleteAll();
 
-            // раскомменитруйте следующие строки
-            // когда создадите репозиторий пользователя
-            // и сервис авторизации
-//            userRepository.deleteAll();
-//
-//            User user1 = new User();
-//            user1.setEmail("test@test");
-//            user1.setPassword(encoder.encode("test"));
-//            userRepository.save(user1);
+            List<Comment> commentList = new ArrayList<>();
+            List<Like> likeList = new ArrayList<>();
+            List<Publication> publicationList = new ArrayList<>();
+            List<Subscription> subscriptionList = new ArrayList<>();
+            List<User> userList = new ArrayList<>();
 
-//            User user2 = new User();
-//            user2.setEmail("guest@test");
-//            user2.setPassword(encoder.encode("guest"));
-//            userRepository.save(user2);
 
-//            User user3 = new User();
-//            user3.setEmail("admin@test");
-//            user3.setPassword(encoder.encode("admin"));
-//            userRepository.save(user3);
+
+
+            User user1 = new User();
+            user1.setEmail("test@test");
+            user1.setPassword(encoder.encode("test"));
+            userRepository.save(user1);
+
+            User user2 = new User();
+            user2.setEmail("guest@test");
+            user2.setPassword(encoder.encode("guest"));
+            userRepository.save(user2);
+
+            User user3 = new User();
+            user3.setEmail("admin@test");
+            user3.setPassword(encoder.encode("admin"));
+            userRepository.save(user3);
 
         };
     }
